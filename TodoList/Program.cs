b1a8b9e226b300585ef165b19b1fab575f8e7c5d
Ghidyon <gideonakunana@gmail.com>
2021-07-07 03:56:36 +0200

    added exception handler

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -6,26 +6,34 @@ namespace TodoList
     {
         static void Main(string[] args)
         {
-            // Prompt user for number of tasks to carry out
-            Console.Write("How many tasks do you want to do? ");
-            string listNo = Console.ReadLine();
-            int listNumber = Convert.ToInt32(listNo);
-
-            // Use listNumber to create array with number of list
-            string[] todoItems = new string[listNumber];
-
-            // Use loop to input list items into todoList array
-            for (int i = 0; i < todoItems.Length; i++)
+            try
             {
-                Console.Write("Enter list item: ");
-                todoItems[i] = Console.ReadLine();
+                // Prompt user for number of tasks to carry out
+                Console.Write("How many tasks do you want to do? ");
+                string listNo = Console.ReadLine();
+                int listNumber = Convert.ToInt32(listNo);
+
+                // Use listNumber to create array with number of list
+                string[] todoItems = new string[listNumber];
+
+                // Use loop to input list items into todoList array
+                for (int i = 0; i < todoItems.Length; i++)
+                {
+                    Console.Write("Enter list item: ");
+                    todoItems[i] = Console.ReadLine();
+                }
+
+                // List out list items inside todoList array
+                Console.WriteLine("Here are your tasks to-do!");
+                foreach (var item in todoItems)
+                {
+                    Console.WriteLine(item);
+                }
             }
-
-            // List out list items inside todoList array
-            Console.WriteLine("Here are your tasks to-do!");
-            foreach (var item in todoItems)
+            catch (FormatException e)
             {
-                Console.WriteLine(item);
+                Console.WriteLine($"FormatException Handler: {e.Message}");
+                Console.WriteLine("Please enter a number");
             }
         }
     }
