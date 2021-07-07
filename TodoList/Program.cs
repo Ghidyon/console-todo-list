24e85e9805682ec839856e00a4576d31967cc198
Ghidyon <gideonakunana@gmail.com>
2021-07-07 03:46:13 +0200

    console todolist

---
+++ TodoList/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TodoList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt user for number of tasks to carry out
+            Console.Write("How many tasks do you want to do? ");
+            string listNo = Console.ReadLine();
+            int listNumber = Convert.ToInt32(listNo);
+
+            // Use listNumber to create array with number of list
+            string[] todoItems = new string[listNumber];
+
+            // Use loop to input list items into todoList array
+            for (int i = 0; i < todoItems.Length; i++)
+            {
+                Console.Write("Enter list item: ");
+                todoItems[i] = Console.ReadLine();
+            }
+
+            // List out list items inside todoList array
+            Console.WriteLine("Here are your tasks to-do!");
+            foreach (var item in todoItems)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
